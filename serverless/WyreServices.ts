3ada7bf77eb5cc4938cd733052ad665b03c97930
Jacob Mortensen <jacob@jacobmortensen.com>
2019-02-16 06:08:01 -0700

    Create frontend and move serverless stuff into subfolder

--- serverless/WyreServices.ts
+++ serverless/WyreServices.ts
@@ -5,11 +5,43 @@ const API_KEY = '';
 const API_SECRET = ''
 
 export class WyreServices {
+  axios;
+  constructor() {
+  }
   getAccount(accountId: string): Promise<AxiosResponse> {
     const endPoint = `/v3/accounts/${accountId}`;
     const url = this.generateUrl(endPoint);
+    return this.getRequest(url);
+  }
+
+  createTransfer(srn: string,
+    sourceCurrencySymbol: string,
+    sourceAmt: string,
+    destSrn: string,
+    destCurrencySymbol: string,
+    message: string) {
+    const endPoint = `/v3/transfers`;
+    const url = this.generateUrl(endPoint);
     const signed = this.signMessage(url, " ");
-    return this.getRequest(url, signed);
+    const params = {
+      // "source": "account:AC-WYUR7ZZ6UMU",
+      // "sourceCurrency": "USD",
+      // "sourceAmount": "5",
+      // "dest": "bitcoin:14CriXWTRoJmQdBzdikw6tEmSuwxMozWWq",
+      // "destCurrency": "BTC",
+      // "message": "Payment for DorianNakamoto@sendwyre.com",
+      // "notifyUrl": "An optional url that Wyre will POST a status callback to ",
+      // "autoConfirm": true
+      "source": srn,
+      "sourceCurrency": sourceCurrencySymbol,
+      "sourceAmount": sourceAmt,
+      "dest": destSrn,
+      "destCurrency": destCurrencySymbol,
+      "message": message,
+      "autoConfirm": true
+
+    }
+    return this.postRequest(url, params);
   }
 
   timestampUrl(url: string): string {
@@ -24,22 +56,35 @@ export class WyreServices {
     return this.timestampUrl(url);
   }
 
-  getRequest(url: string, signature: string): Promise<AxiosResponse> {
+  getRequest(url: string): Promise<AxiosResponse> {
     const signed = this.signMessage(url, "");
     return new Promise((resolve, reject) => {
-      const instance = axios.create({
-        headers: {
-          'X-Api-Key': API_KEY,
-          'X-Api-Signature': signed,
-        }
-      });
-      instance.get(url, {
-      }).then((result) => {
+      axios.get(url, this.getHeaders(signed)).then((result) => {
         resolve(result);
       }).catch((e) => {
         reject(e);
       });
     });
+  }
+
+  postRequest(url: string, params: Object): Promise<AxiosResponse> {
+    const signed = this.signMessage(url, "");
+    return new Promise((resolve, reject) => {
+      axios.post(url, params, this.getHeaders(signed)).then((result) => {
+        resolve(result);
+      }).catch((e) => {
+        reject(e);
+      });
+    });
+  }
+
+  getHeaders(signature: string) {
+    return {
+      headers: {
+        'X-Api-Key': API_KEY,
+        'X-Api-Signature': signature,
+      }
+    }
   }
 
   signMessage(url: string, body: string): string {
