b5334cab4affb9c698bd3cdfaaa50210bd88f70b
Jacob Mortensen <jacob@jacobmortensen.com>
2019-02-16 01:45:47 -0700

    Move funcs out to a class

---
+++ WyreServices.ts
@@ -0,0 +1,73 @@
+import axios, { AxiosResponse } from 'axios';
+import { HmacSHA256 } from 'crypto-js';
+
+const API_KEY = '';
+const API_SECRET = ''
+
+export class WyreServices {
+  getAccount(accountId: string): Promise<AxiosResponse> {
+    const endPoint = `/v3/accounts/${accountId}`;
+    const url = this.generateUrl(endPoint);
+    const urlTimestamped = this.timestampUrl(url);
+    const signed = this.signMessage(url, " ");
+    return this.getRequest(url, urlTimestamped, signed);
+
+    // return new Promise((resolve, reject) => {
+    //   const instance = axios.create({
+    //     headers: {
+    //       'X-Api-Key': API_KEY,
+    //       'X-Api-Signature': signed,
+    //     }
+    //   });
+    //   instance.get(urlTimestamped, {
+    //   }).then((result) => {
+    //     resolve(result);
+    //   }).catch((e) => {
+    //     reject(e);
+    //   });
+    // });
+  }
+
+  timestampUrl(url: string): string {
+    // Additionally, you should include a GET parameter named timestamp which is the current time in millisecond epoch format. We use this timestamp to help protect against replay attacks.
+    return `${url}?timestamp=${(new Date).getTime()}`;
+  }
+
+  generateUrl(restEndpoint: string): string {
+    const baseURL = 'https://api.testwyre.com';
+    const url = `${baseURL}${restEndpoint}`;
+    // TODO it seems like timestamp isn't included in the URL which is hashed but I can't get it to work either way
+    return url;
+  }
+
+  getRequest(url: string, urlTimestamped: string, signature: string): Promise<AxiosResponse> {
+    const signed = this.signMessage(url, " ");
+    return new Promise((resolve, reject) => {
+      const instance = axios.create({
+        headers: {
+          'X-Api-Key': API_KEY,
+          'X-Api-Signature': signed,
+        }
+      });
+      instance.get(urlTimestamped, {
+      }).then((result) => {
+        resolve(result);
+      }).catch((e) => {
+        reject(e);
+      });
+    });
+  }
+
+  signMessage(url: string, body: string): string {
+    /* Calculating the X-Api-Signature field is a two step process:
+    1. Concatenate the request URL with the body of the HTTP request (encoded using UTF8) into a string. Use an empty string for the HTTP body in GET requests
+    2. Compute the signature as a HEX encoded HMAC with SHA-256 and your API Secret Key
+    Note: You must send the request body exactly as you sign it, whitespace and all. The server calculates the signature based on exactly what's in the request body.
+    https://docs.sendwyre.com/docs/authentication#section-calculating-the-request-signature
+    */
+    const urlEnc = Buffer.from(`${url}${body}`).toString('base64');
+    const hmaced = HmacSHA256(urlEnc, API_SECRET).toString();
+    return hmaced;
+    //return new Buffer(hmaced).toString('hex');
+  }
+}
--- handler.ts
+++ handler.ts
@@ -1,13 +1,11 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
-import axios, { AxiosResponse } from 'axios';
-import { HmacSHA256 } from 'crypto-js';
+import { WyreServices } from './WyreServices';
 
-const API_KEY = '';
-const API_SECRET = ''
 const ACCOUNT_ID = '';
 export const hello: APIGatewayProxyHandler = async (event, context) => {
   try {
-    await calllApi(ACCOUNT_ID).then((result) => {
+    let wyreService = new WyreServices();
+    await wyreService.getAccount(ACCOUNT_ID).then((result) => {
       console.log(result);
     });
   } catch (e) {
@@ -22,39 +20,4 @@ export const hello: APIGatewayProxyHandler = async (event, context) => {
       input: event,
     }),
   }
-}
-
-function calllApi(accountId: string): Promise<AxiosResponse> {
-  const baseURL = 'https://api.testwyre.com';
-  const endPoint = `/v3/accounts/${accountId}`;
-  const url = `${baseURL}${endPoint}`;
-  // Additionally, you should include a GET parameter named timestamp which is the current time in millisecond epoch format. We use this timestamp to help protect against replay attacks.
-  const urlTimestamped = `${url}?timestamp=${(new Date).getTime()}`;
-  const signed = signMessage(url, "");
-  return new Promise((resolve, reject) => {
-    const instance = axios.create({
-      headers: {
-        'X-Api-Key': API_KEY,
-        'X-Api-Signature': signed,
-      }
-    });
-    instance.get(urlTimestamped, {
-    }).then((result) => {
-      resolve(result);
-    }).catch((e) => {
-      reject(e);
-    });
-  });
-}
-
-function signMessage(url: string, body: string): string {
-  /* Calculating the X-Api-Signature field is a two step process:
-  1. Concatenate the request URL with the body of the HTTP request (encoded using UTF8) into a string. Use an empty string for the HTTP body in GET requests
-  2. Compute the signature as a HEX encoded HMAC with SHA-256 and your API Secret Key
-  Note: You must send the request body exactly as you sign it, whitespace and all. The server calculates the signature based on exactly what's in the request body.
-  https://docs.sendwyre.com/docs/authentication#section-calculating-the-request-signature
-  */
-  const urlEnc = Buffer.from(`${url}${body}`).toString('base64');
-  const hmaced = HmacSHA256(urlEnc, API_SECRET).toString();
-  return new Buffer(hmaced).toString('hex');
 }
