d5e8319873a1c2394f523cbb18d84355e51d7464
Jacob Mortensen <jacob@jacobmortensen.com>
2019-02-16 00:50:41 -0700

    First stab at signing a request...it doesn't work but seems like it should

--- handler.ts
+++ handler.ts
@@ -1,10 +1,12 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import axios, { AxiosResponse } from 'axios';
-const API_ID = '';
-const API_SECRET = '';
+import { HmacSHA256 } from 'crypto-js';
+
+const API_KEY = '';
+const API_SECRET = ''
 const ACCOUNT_ID = '';
 export const hello: APIGatewayProxyHandler = async (event, context) => {
-  await calllApi().then((result) => {
+  await calllApi(ACCOUNT_ID).then((result) => {
     console.log(result);
   });
   return {
@@ -16,17 +18,35 @@ export const hello: APIGatewayProxyHandler = async (event, context) => {
   }
 }
 
-function calllApi(): Promise<AxiosResponse> {
+function calllApi(accountId: string): Promise<AxiosResponse> {
+  const baseURL = 'https://api.sendwyre.com';
+  const endPoint = `/v3/accounts/${accountId}?X-Api-Key=${API_SECRET}&X-Api-Signature`;
+  const url = `${baseURL}${endPoint}`;
+  const signed = signMessage(url, "");
+  console.log(signed);
   return new Promise((resolve, reject) => {
     const instance = axios.create({
-      baseURL: 'https://api.sendwyre.com',
-      headers: { 'X-Custom-Header': API_ID }
+      headers: {
+        'X-Custom-Header': API_KEY,
+        'X-Api-Signature': signed
+      }
     });
-    instance.get(`/v3/accounts/${ACCOUNT_ID}?X-Api-Key=${API_SECRET}&X-Api-Signature`, {
+    instance.get(url, {
     }).then((result) => {
       resolve(result);
     }).catch((e) => {
       reject(e);
     });
   });
+}
+
+function signMessage(url: string, body: string): string {
+  /* Calculating the X-Api-Signature field is a two step process:
+  1. Concatenate the request URL with the body of the HTTP request (encoded using UTF8) into a string. Use an empty string for the HTTP body in GET requests
+  2. Compute the signature as a HEX encoded HMAC with SHA-256 and your API Secret Key
+  Note: You must send the request body exactly as you sign it, whitespace and all. The server calculates the signature based on exactly what's in the request body.
+*/
+  const urlEnc = Buffer.from(`${url}${body}`).toString('base64');
+  const hmaced = HmacSHA256(urlEnc, API_SECRET).toString();
+  return new Buffer(hmaced).toString('hex');
 }
