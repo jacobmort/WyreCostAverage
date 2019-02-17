fdd3be34a47ee080f80c307767f68973b06f6c38
Jacob Mortensen <jacob@jacobmortensen.com>
2019-02-16 21:23:24 -0700

    Refactor all calls to wyreservice into 1 method

--- serverless/account.ts
+++ serverless/account.ts
@@ -6,21 +6,45 @@ const SUB_ACCOUNT_ID = '';
 
 export const handler: APIGatewayProxyHandler = async (event, context) => {
   let httpMethod = event["httpMethod"];
-  console.log(httpMethod);
-  if (httpMethod === 'POST') {
-    return await post(event);
-  } else if (event["path"] === 'account/document') {
-    return await postDocument(event);
-  } else {
-    return await get(event);
-  }
+  //return await call("getAccount", [SUB_ACCOUNT_ID, true]);
+  // return call("getPaymentMethod", ['PA_42VJMPZH7UZ', SUB_ACCOUNT_ID, true]);
+  return await call("transfer", [
+    "paymentmethod:PA_42VJMPZH7UZ",
+    "USD",
+    "10.0",
+    `account:${SUB_ACCOUNT_ID}`,
+    "USD",
+    "test"]);
+  // if (httpMethod === 'POST') {
+  //   if (event["path"] === 'account/document') {
+  //     return await call("postDocument", [event['body']]);
+  //   } else if (event["path"] === 'account/payment') {
+  //     return await call("addPaymentMethod", [accountId, paymentId]);
+  //   } else {
+  //     return await call("create", [
+  //   event['body']['accountId'],
+  //     event['body']['country'],
+  //     event['body']['fullName'],
+  //     event['body']['email'],
+  //     event['body']['cellphone'],
+  //     event['body']['street1'],
+  //     event['body']['street2'],
+  //     event['body']['city'],
+  //     event['body']['state'],
+  //     event['body']['postalCode'],
+  //     event['body']['ssn'],
+  //     event['body']['dob']
+  // ]);
+  //   }
+  // } else {
+  //   return await call("getAccount", [SUB_ACCOUNT_ID, true]);
+  // }
 }
 
-async function get(event) {
+async function call(wyreServiceMethod: string, args) {
   try {
     let wyreService = new WyreServices();
-    let accountId = ACCOUNT_ID;
-    await wyreService.getAccount(accountId, true).then((result) => {
+    await wyreService[wyreServiceMethod](...args).then((result) => {
       console.log(JSON.stringify(result.data));
       return {
         statusCode: 200,
@@ -37,57 +61,4 @@ async function get(event) {
   }
 }
 
-async function post(event) {
-  try {
-    let wyreService = new WyreServices();
-    await wyreService.createAccount(
-      event['body']['accountId'],
-      event['body']['country'],
-      event['body']['fullName'],
-      event['body']['email'],
-      event['body']['street1'],
-      event['body']['street2'],
-      event['body']['city'],
-      event['body']['state'],
-      event['body']['postalCode'],
-      event['body']['ssn'],
-      event['body']['dob'] //YYYY-MM-DD
-    ).then((result) => {
-      console.log(JSON.stringify(result.data));
-      return {
-        statusCode: 200,
-        body: JSON.stringify(result.data),
-      }
-    });
-  } catch (e) {
-    console.log('error caught');
-    console.log(e);
-    return {
-      statusCode: 500,
-      body: JSON.stringify(e.message),
-    }
-  }
-}
-async function postDocument(event) {
-  try {
-    let wyreService = new WyreServices();
-    await wyreService.uploadDocument(
-      ACCOUNT_ID,
-      event['body'],
-      true
-    ).then((result) => {
-      console.log(JSON.stringify(result.data));
-      return {
-        statusCode: 200,
-        body: JSON.stringify(result.data),
-      }
-    });
-  } catch (e) {
-    console.log('error caught');
-    console.log(e);
-    return {
-      statusCode: 500,
-      body: JSON.stringify(e.message),
-    }
-  }
-}
+
